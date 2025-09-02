e30e31709e322ded8811144d3becf10c01ced056
Ethan Biggar <biggar4@iccc.cc.ia.us>
2025-09-02 10:33:14 -0500

    Initial Commit

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyFirstProject
+//
+//  Created by Ethan Biggar on 9/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
